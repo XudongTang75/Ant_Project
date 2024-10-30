0448eacdfb781d60dce33c57e1f2d647c4aaacd9
Xudong Tang <xudongtang75@gmail.com>
2024-10-29 20:29:45 -0500

    can change joint position, can do random action, cannot do controlled action to move

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ import warp as wp
 import warp.examples
 import warp.sim
 import warp.sim.render
-import copy
 
 
 # Return ants that align on X axis, and for y axis they are 5.0 apart.
@@ -87,19 +86,27 @@ class Example:
         # timestep for each substep
         self.sim_dt = self.frame_dt / self.sim_substeps
 
+        self.step_size = 0.2
+
         self.num_envs = num_envs
-        offsets = compute_env_offsets(self.num_envs)
+        self.offsets = compute_env_offsets(self.num_envs)
         self.dof = len(articulation_builder.joint_q)
 
+        self.target_origin = []
         for i in range(self.num_envs):
-            builder.add_builder(articulation_builder, xform=wp.transform(offsets[i], wp.quat_identity()))
+            builder.add_builder(articulation_builder, xform=wp.transform(self.offsets[i], wp.quat_identity()))
 
             builder.joint_q[-8:] = [0.0, 1.0, 0.0, -1.0, 0.0, -1.0, 0.0, 1.0]
             builder.joint_axis_mode = [wp.sim.JOINT_MODE_TARGET_POSITION] * len(builder.joint_axis_mode)
-            builder.joint_act[-8:] = [0.0, 1.0, 0.0, -1.0, 0.0, -1.0, 0.0, 1.0]
+            builder.joint_act[-8:] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+            #self.target_origin.append((0.4, 0.3, self.offsets[self.num_envs-1-i,1]+0.4))
+
+        #self.target_origin = np.array(self.target_origin)
 
 
         np.set_printoptions(suppress=True)
+        
 
         self.model = builder.finalize()
         self.model.ground = True
@@ -107,6 +114,12 @@ class Example:
         self.model.body_q.requires_grad = True
         self.model.joint_attach_ke = 16000.0
         self.model.joint_attach_kd = 200.0
+
+
+        ##################################################################################################
+        #                        This is where we change the Joint action mode                           #
+        ##################################################################################################
+        self.model.joint_axis_mode = wp.array([warp.sim.JOINT_MODE_FORCE for _ in range(self.dof)], dtype=wp.int32)
 
         self.integrator = wp.sim.FeatherstoneIntegrator(self.model)
 
@@ -118,32 +131,18 @@ class Example:
         self.state_0 = self.model.state(requires_grad=True)
         self.state_1 = self.model.state(requires_grad=True)
         
+        self.control = self.model.control()
 
         self.num_links = len(articulation_builder.joint_type)
         # FIXME
         # Might need to change to [2,4,6,8] for 4 feets
-        self.ee_link_index = 4
+        self.ee_link_index = 8
         # FIXME
         # Currently no offset, might need to change
         self.ee_link_offset = wp.vec3(0.0, 0.0, 0.0)
         self.ee_pos = wp.zeros(self.num_envs, dtype=wp.vec3, requires_grad=True)
         
-        #tape_1 = wp.Tape()
-        #with tape_1:
-        #    wp.sim.eval_fk(self.model, self.model.joint_q, self.model.joint_qd, None, self.state_0)
-
-        # FIXME
-        # Might not want to call it here
-        # It seems like the end effector position is a relative position wrt its body
-        # Try add 0.01 to the x axis of ee_pos for now
-        '''
-        tape = wp.Tape()
-        with tape:
-            self.compute_ee_position()
-        self.target_origin = self.ee_pos
-        self.target_origin = np.array(self.target_origin)
         self.targets = self.target_origin.copy()
-        '''
 
         self.profiler = {}
 
@@ -165,61 +164,78 @@ class Example:
             outputs=[self.ee_pos],
         )
 
-    def compute_jacobian(self):
+    # if multiple envs, change z value 
+    # ee start position for joint #0: [0, 0.3, 0]
+    # ee start position for joint #1: [0.2, 0.3, -0.2]
+    # ee start position for joint #2: [0.4, 0.3, -0.4]
+    # ee start position for joint #3: [-0.2, 0.3, -0.2]
+    # ee start position for joint #4: [-0.4, 0.3, -0.4]
+    # ee start position for joint #5: [-0.2, 0.3, 0.2]
+    # ee start position for joint #6: [-0.4, 0.3, 0.4]
+    # ee start position for joint #7: [0.2, 0.3, 0.2]
+    # ee start position for joint #8: [0.4, 0.3, 0.4]
 
-        # jacobians -> [num_envs][3][15]
+    
 
-        #print(jacobians)
-        return jacobians
-
-
-
-    def simulate(self):
+    def simulate(self, frame_num):
         for _ in range(self.sim_substeps):
             self.state_0.clear_forces()
-
-
-            
-            # self.state_0.joint_q = wp.array(
-            #     self.state_0.joint_q.numpy() + [0,0,0,0,0,0,0,0.01,0,0,0,0,0,0,0,
-            #                                     0,0,0,0,0,0,0,0.01,0,0,0,0,0,0,0],
-            #     dtype=wp.float32,
-            #     requires_grad=True,
-            # ) 
-            
-
-            #self.compute_jacobian()
             #print(self.ee_pos)
+            #print(self.target_origin)
             tape = wp.Tape()
             with tape:
                 wp.sim.collide(self.model, self.state_0)
-                self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt)
+                self.integrator.simulate(self.model, self.state_0, self.state_1, self.sim_dt, control=self.control)
                 self.compute_ee_position()
-                print(self.ee_pos)
+                #print(self.state_0.joint_q)
                 jacobians = np.empty((self.num_envs, 3, self.dof), dtype=np.float32)
                 for output_index in range(3):
                     # select which row of the Jacobian we want to compute
                     select_index = np.zeros(3)
                     select_index[output_index] = 1.0
                     e = wp.array(np.tile(select_index, self.num_envs), dtype=wp.vec3)
-                    #print(e)
                     tape.backward(grads={self.ee_pos: e})
-                    if self.state_0.joint_q in tape.gradients:
-                        q_grad_i = tape.gradients[self.state_0.joint_q]
-                    else:
-                        print("joint_q gradients not found in tape.")
+                    q_grad_i = tape.gradients[self.state_0.joint_q]
                     jacobians[:, output_index, :] = q_grad_i.numpy().reshape(self.num_envs, self.dof)
                     tape.zero()
-                #print(jacobians)
+        
             self.state_0, self.state_1 = self.state_1, self.state_0
 
+            
+            if frame_num > 60:
+                
+                ##################################################################################################
+                #             Generate RANDOM action input, eventually want to do controlled action              #
+                ##################################################################################################
+                self.control.joint_act = wp.array(np.random.uniform(-500, 500, size=8).astype(np.float32))
+
+
+
+                ##################################################################################################
+                #                This part update joint_q using jacobian wrt the ee_position                     #
+                # It does NOT have any action involved, so the code simply render the joint at updated location! #
+                ##################################################################################################
+                '''
+                error = np.tile(np.array([0.008, 0, 0]), (self.num_envs, 1))
+
+                self.error = error.reshape(self.num_envs, 3, 1)
+                # compute Jacobian transpose update
+                delta_q = np.matmul(jacobians.transpose(0, 2, 1), self.error)
+                self.state_0.joint_q = wp.array(
+                    self.state_0.joint_q.numpy() + self.step_size * delta_q.flatten(),
+                    dtype=wp.float32,
+                    requires_grad=True,
+                )
+                '''
+                
+
     
-    def step(self):
+    def step(self, frame_num):
         with wp.ScopedTimer("step", print=False):
             if self.use_cuda_graph:
                 wp.capture_launch(self.graph)
             else:
-                self.simulate()
+                self.simulate(frame_num)
         self.sim_time += self.frame_dt
         
 
@@ -251,9 +267,13 @@ if __name__ == "__main__":
 
     with wp.ScopedDevice(args.device):
         example = Example(stage_path=args.stage_path, num_envs=args.num_envs)
+        
 
-        for _ in range(args.num_frames):
-            example.step()
+        for i in range(args.num_frames):
+            #if i > 60:
+            #    example.targets = example.ee_pos.numpy().copy()
+            #    example.targets[:,0] += 0.0002
+            example.step(i)
             example.render()
 
         if example.renderer:
